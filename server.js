15cb2028cd0162dacbaca4be64c13ef8c0e32ba2
Hanna Boratynska <boratynskahanna@gmail.com>
2021-08-29 10:43:04 +0400

    Add endpoints

--- server.js
+++ server.js
@@ -4,17 +4,62 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
-
-app.use(cors({
-    "origin": "https://kodilla.com", //origin sets domains that we approve
-    "methods": "GET,POST", //we allow only GET and POST methods
-}));
 
 const db = [
     { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
     { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
 ];  
+
+app.get('/testimonials', (req, res) => {
+  res.json(db);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+  const item = db.find(item => item.id == req.params.id);
+  if ( item ) res.json(item);
+  else res.status(404).json({ message: 'Not found...' });
+});
+
+app.get('/testimonials/random', (req, res) => {
+  const i = Math.floor(Math.random() * db.length);
+  res.json(db[i]);
+});
+
+app.post('/testimonials', (req, res) => {
+  const { author, text } = req.body;
+
+  if (author && text) {
+    const id = uuidv4();
+    db.push({id, author, text});
+    res.json({ message: 'OK' });
+  }
+  else res.status(404).json({ message: 'Not found...' });
+});
+
+app.put('/testimonials/:id', (req, res) => {
+  const item = db.find(item => item.id == req.params.id);
+  const { author, text } = req.body;
+
+  if (item && author && text) {
+    item.author = author;
+    item.text = text;
+    res.json({ message: 'OK' });
+  }
+  else res.status(404).json({ message: 'Not found...' });
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+  const item = db.find(item => item.id == req.params.id);
+  if (item) {
+    db.splice(db.indexOf(item), 1);
+    res.json({ message: 'OK' });
+  }
+  else res.status(404).json({ message: 'Not found...' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
+});
 
 
 app.listen(8000, () => {
